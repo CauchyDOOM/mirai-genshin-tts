5d135391a9477799f9ff97703f5698518f5eb36c
水纹霖霖 <67990349+CauchyDOOM@users.noreply.github.com>
2022-08-24 22:15:03 +0800

    Update YaeMikoSource.java

--- src/main/java/com/cauchydoom/MiraiGenshinTTS/musicsource/YaeMikoSource.java
+++ src/main/java/com/cauchydoom/MiraiGenshinTTS/musicsource/YaeMikoSource.java
@@ -9,7 +9,7 @@ public class YaeMikoSource implements MusicSource {
 	@Override
 	public MusicInfo get(String keyword) throws Exception {
 		keyword=Utils.urlEncode(keyword);
-		return new MusicInfo("GenshinTTS", "YaeMiko", "https://i2.hdslb.com/bfs/face/d2a95376140fb1e5efbcbed70ef62891a3e5284f.jpg", "http://233366.proxy.nscc-gz.cn:8888/?speaker=" + Utils.urlEncode("八重神子") + "&text=" + keyword, "http://233366.proxy.nscc-gz.cn:8888/?speaker=" + Utils.urlEncode("八重神子") + "&text=" + keyword, "酷狗","",205141);
+		return new MusicInfo("GenshinTTS", "YaeMiko", "https://i2.hdslb.com/bfs/face/d2a95376140fb1e5efbcbed70ef62891a3e5284f.jpg", "http://233366.proxy.nscc-gz.cn:8888/?speaker=" + Utils.urlEncode("八重神子") + "&text=" + keyword, "http://233366.proxy.nscc-gz.cn:8888/?speaker=" + Utils.urlEncode("八重神子") + "&text=" + keyword, "TapTap","",1105676484);
 	}
 
 }
