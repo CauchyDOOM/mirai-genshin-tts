ec6320990ce1a9fd792f926c088ef028b5b44e86
水纹霖霖 <67990349+CauchyDOOM@users.noreply.github.com>
2022-08-26 00:45:48 +0800

    Add files via upload

---
+++ src/main/java/com/cauchydoom/MiraiGenshinTTS/musicsource/DilucSource.java
@@ -0,0 +1,15 @@
+package com.cauchydoom.MiraiGenshinTTS.musicsource;
+
+import com.cauchydoom.MiraiGenshinTTS.MusicInfo;
+import com.cauchydoom.MiraiGenshinTTS.MusicSource;
+import com.cauchydoom.MiraiGenshinTTS.Utils;
+
+public class DilucSource implements MusicSource {
+
+	@Override
+	public MusicInfo get(String keyword) throws Exception {
+		keyword=Utils.urlEncode(keyword);
+		return new MusicInfo("GenshinTTS", "Diluc", "https://i2.hdslb.com/bfs/face/d2a95376140fb1e5efbcbed70ef62891a3e5284f.jpg", "http://233366.proxy.nscc-gz.cn:8888/?speaker=" + Utils.urlEncode("迪卢克") + "&text=" + keyword, "http://233366.proxy.nscc-gz.cn:8888/?speaker=" + Utils.urlEncode("迪卢克") + "&text=" + keyword, "原神", "", 100495085);
+	}
+
+}
